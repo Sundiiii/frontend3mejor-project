9826fac90c7ab09fb8d7ce149b3dede939df0f42
Sundar  Deshmukh <109438264+Sundiiii@users.noreply.github.com>
2023-07-02 02:19:24 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+const signup1=document.getElementById("signup1");
+signup1.addEventListener('click',()=>{
+    location.href="../signup";
+});
+const login1=document.getElementById("login1");
+login1.addEventListener('click',()=>{
+    location.href="../loginpage";
+})
